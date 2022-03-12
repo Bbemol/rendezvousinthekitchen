4902e708341f249b126031de51580c7d7d5fcc3e
Benjamin PERNEY <benjperney@gmail.com>
2022-03-12 10:07:21 +0100

    feat: animation menu items

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['_site/**/*.html', '*.js'],
   theme: {
     extend: {
+      transitionTimingFunction: {
+        'menu': 'cubic-bezier(.45,.33,.53,.86)'
+      },
       fontFamily: {
         mono: ['necto_monoregular', 'sans-serif'],
         serif: ['cantata_oneregular', 'serif']
